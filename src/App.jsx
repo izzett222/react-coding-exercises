ac9cf02a5cc9bd9491884a84e663a9a0dc9654fe
izzett222 <iizzeddin62@gmail.com>
2023-03-02 12:08:15 +0200

    add button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Square from "./components/Square";
-import Alert from "./components/Alert";
+import Button from "./components/Button";
 
 function App() {
   return (
-    <div className="h-screen flex justify-center items-center">
-      <Alert />
+    <div className="h-screen flex justify-center items-center gap-3">
+      <Button name={1}>button 1</Button>
+      <Button name={2}>button 2</Button>
+      <Button name={3}>button 3</Button>
     </div>
   );
 }
