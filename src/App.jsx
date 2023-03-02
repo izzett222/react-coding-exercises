c31376e93a613e2c45032f13df67dc0f399b21b1
izzett222 <iizzeddin62@gmail.com>
2023-03-02 11:58:52 +0200

    add yellow  square component

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Square from "./components/Square";
+
+function App() {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <Square />
+    </div>
+  );
+}
+
+export default App;
