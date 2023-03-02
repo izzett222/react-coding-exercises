dad1ae855519068e8e703dbe904837fc4a83964d
izzett222 <iizzeddin62@gmail.com>
2023-03-02 12:43:23 +0200

    add form component

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import Button from "./components/Button";
 import Counter from "./components/Counter";
 import List from "./components/List";
 import FancyList from "./components/FancyList";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div className="h-screen flex justify-center items-center gap-3">
-      <FancyList />
+      <Form />
     </div>
   );
 }
---
+++ src/components/Form.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+export default function Form() {
+  const [data, setData] = useState({ firstname: "", lastName: "" });
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setData({ ...data, [name]: value });
+  }
+  function handleSubmit(event) {
+    event.preventDefault()
+    alert(`Hello ${data.firstname} ${data.lastName}`)
+  }
+  return <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
+    <input type="text" className="border border-gray-600 rounded-md w-80 h-9 indent-2" name="firstname" placeholder="First name" value={data.firstname} onChange={handleChange} />
+    <input type="text" className="border border-gray-600 rounded-md w-80 h-9 indent-2" name="lastName" value={data.lastName} placeholder="Last name" onChange={handleChange} />
+    <button className="bg-red-500 px-4 py-2 text-white text-xl uppercase rounded-md">greet me</button>
+  </form>
+}
